7ba274a5c8bd64f045ebd0835bf725dc6138de0f
zhoudejia <152068663+Zvezda117@users.noreply.github.com>
2026-09-15 13:02:54 +0800

    types: expose Ascend runtime surface

--- python/gaffa/__init__.pyi
+++ python/gaffa/__init__.pyi
@@ -2,11 +2,10 @@ from . import dedispersion as dedispersion
 from . import ffa as ffa
 from . import io as io
 from . import pfold as pfold
-from ._core import cuda_device_count, cuda_runtime_version, vector_add
+from ._core import ascend_device_count, vector_add
 
 __all__ = [
-    "cuda_device_count",
-    "cuda_runtime_version",
+    "ascend_device_count",
     "vector_add",
     "dedispersion",
     "ffa",
